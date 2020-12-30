495f2ff21f7fbaf175de9d0d609c653a2e0a5740
Stephan <golubevstephan@gmail.com>
2020-12-30 13:05:20 +0300

    Сайт готов

---
+++ blog/templatetags/blog_tags.py
@@ -0,0 +1,30 @@
+from django import template
+from ..models import Post
+from django.utils.safestring import mark_safe
+import markdown
+
+register = template.Library()
+
+
+@register.simple_tag
+def total_posts():
+    x = Post.objects.all().filter(status='published').count()
+    return x
+
+@register.inclusion_tag('blog/post/latest_posts.html')
+def show_latest_posts(count=5):
+    latest_posts = Post.objects.all().filter(status='published').order_by('-publish')[:2]
+    return {'latest_posts': latest_posts}
+
+from django.db.models import Count
+
+@register.simple_tag()
+def get_most_commented_posts(count=5):
+    return Post.objects.all().filter(status='published').annotate(
+        total_comments=Count('comments')
+    ).order_by('-total_comments')[:2]
+
+
+@register.filter(name='markdown')
+def markdown_format(text):
+    return mark_safe(markdown.markdown(text))
